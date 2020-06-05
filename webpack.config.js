9fe7cf87e8be7c1485dd90c6a1667164f68addfb
wataru <dossjakapoko@gmail.com>
2020-06-05 21:16:56 +0900

    LoadImages with fileloader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ module.exports = {
             loader: "css-loader"
           }
         ]
+      },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "images/[name].[ext"
+            }
+          }
+        ]
       }
     ]
   },
