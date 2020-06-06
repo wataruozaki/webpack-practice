106cbf9a2e2f25cb5bf4de35ce47e6d5afe74760
wataru <dossjakapoko@gmail.com>
2020-06-06 11:45:04 +0900

    Generate Html though Pug

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,21 @@ module.exports = {
             }
           }
         ]
+      },
+
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader"
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true
+            }
+          }
+        ]
       }
     ]
   },
@@ -40,7 +55,12 @@ module.exports = {
       filename: "./stylesheets/main.css"
     }),
     new HtmlwebpackPlugin({
-      template: "./src/templates/index.html"
+      template: "./src/templates/index.pug",
+      filename: "index.html"
+    }),
+    new HtmlwebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: "access.html"
     }),
     new CleanWebpackPlugin()
   ]
